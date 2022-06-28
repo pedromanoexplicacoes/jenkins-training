d94f2b5298c217397256e1b926a1b7b26230ed7e
Rafael Afonso <pedro.mano.explicacoes@gmail.com>
2022-06-28 20:31:24 +0100

    adicionar teste a falhar

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import sys
 from junit_xml import TestSuite, TestCase
 
 
+def runTests_failures():
+    test_cases = [
+        TestCase('Test1', 'funcional', 123.345, 'Output do teste', 'Falhou falhou!!!!!', False),
+        TestCase('Test2', 'funcional', 33.24, 'Output 2', None, True),
+        TestCase('Test1', 'Integracao', 5, 'Ok Integracao', None, True),
+        ]
+    ts = TestSuite("Test Suite", test_cases)
+    with open('output.xml', 'w') as f:
+        TestSuite.to_file(f, [ts], prettyprint=False)
+
 def runTests():
     test_cases = [
         TestCase('Test1', 'funcional', 123.345, 'Output do teste', None, True),
@@ -20,6 +30,10 @@ def main():
 
     if sys.argv[1] == "runtests":
         runTests()
+        exit(0)
+    
+    if sys.argv[1] == "runtests_f":
+        runTests_failures()
         exit(0)
     
     exit(1)
