9988c69033bf0a87f6641cb05aac46be466a5e3f
Rafael Afonso <pedro.mano.explicacoes@gmail.com>
2022-06-28 10:01:10 +0100

    added main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hi, from Python !!")
+import sys 
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("Hi, from Python !!")
+        return 1
+
+if __name__ == "__main__":
+    main()
