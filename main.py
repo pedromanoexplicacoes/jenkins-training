7bf3333f1d23e2b60db6247bb8957b58c3731b2c
Rafael Afonso <pedro.mano.explicacoes@gmail.com>
2022-06-28 16:54:21 +0100

    adicionado geracao test cases

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import sys 
+from junit_xml import TestSuite, TestCase
 
+
+def runTests():
+    test_cases = [TestCase('Test1', 'some.class.name', 123.345, 'I am stdout!', 'I am stderr!')]
+    ts = TestSuite("my test suite", test_cases)
+    with open('output.xml', 'w') as f:
+        TestSuite.to_file(f, [ts], prettyprint=False)
 
 def main():
     if len(sys.argv) == 1:
         print("Hi, from Python !!")
         exit(0)
 
+    if sys.argv[1] == "runtests":
+        runTests()
+        exit(0)
+    
     exit(1)
 
 if __name__ == "__main__":
